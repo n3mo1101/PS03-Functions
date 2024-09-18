d29779f5adf9fb7a13cb999e930fe1e30e1c065c
n3mo1101 <rd.cruz1101@gmail.com>
2024-09-18 10:58:41 +0800

    Update
    Task 1 : Area of a triangle calculator (Complete).
    Task 2 : Leap year checker (Complete).

---
+++ PS03_1_AreaCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate the area of a triangle.");
+            
+            double[] sides = new double[3];
+            for (int i = 0; i < sides.Length; i++)
+            {
+                Console.Write($"Length of side {i+1}: ");
+                sides[i] = double.Parse(Console.ReadLine());
+            }
+
+            double area = GetArea(sides[0], sides[1], sides[2]);
+            Console.WriteLine($"Area = {Math.Round(area, 2)}");  
+
+            Console.ReadKey();
+        }
+
+
+        static double GetArea(double a, double b, double c)
+        {
+            double s = (a + b + c ) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area; 
+        }
+    }
+}
---
+++ PS03_2_LeapYear/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            bool check = LeapYear(year);
+            if (check == true)
+                Console.WriteLine($"{year} is a leap year.");
+            else
+                Console.WriteLine($"{year} is NOT a leap year.");
+
+            Console.ReadKey();
+        }
+
+        static bool LeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0 && year % 400 != 0)
+                    return false;
+                return true;
+            }
+            return false;
+        }
+    }
+}
